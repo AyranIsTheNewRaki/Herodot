8ff839a2021f3c3e3059443bf81c4b6b5495d07f
Emre Bolat <emre@emre.me>
2017-03-20 00:47:24 +0300

    Backend project created with token based auth support.

---
+++ Backend/src/main/java/world/herodot/rest/PersonRestService.java
@@ -0,0 +1,37 @@
+package world.herodot.rest;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Bogazici University - Spring'17
+ * Herodot - SWE 574 Project
+ * https://github.com/AyranIsTheNewRaki/Herodot
+ */
+
+@RestController
+public class PersonRestService {
+    private static final List<Person> persons;
+
+    static {
+        persons = new ArrayList<>();
+        persons.add(new Person("Emre", "emre@herodot.world"));
+    }
+
+    @RequestMapping(path = "/persons", method = RequestMethod.GET)
+    public static List<Person> getPersons() {
+        return persons;
+    }
+
+    @RequestMapping(path = "/persons/{name}", method = RequestMethod.GET)
+    public static Person getPerson(@PathVariable("name") String name) {
+        return persons.stream()
+                .filter(person -> name.equalsIgnoreCase(person.getName()))
+                .findAny().orElse(null);
+    }
+}
