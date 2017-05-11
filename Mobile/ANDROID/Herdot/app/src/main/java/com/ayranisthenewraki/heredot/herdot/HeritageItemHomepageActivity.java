7fc27ec9041446df47018ebac6f8207f91a69b70
idilgun <idilgun@gmail.com>
2017-05-11 16:31:52 +0300

    add list view, needs to be filled with real data

--- Mobile/ANDROID/Herdot/app/src/main/java/com/ayranisthenewraki/heredot/herdot/HeritageItemHomepageActivity.java
+++ Mobile/ANDROID/Herdot/app/src/main/java/com/ayranisthenewraki/heredot/herdot/HeritageItemHomepageActivity.java
@@ -10,9 +10,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ExpandableListView;
+
+import com.ayranisthenewraki.heredot.herdot.model.CulturalHeritageObject;
+import com.ayranisthenewraki.heredot.herdot.model.ExpandableListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class HeritageItemHomepageActivity extends AppCompatActivity {
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, CulturalHeritageObject> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,17 @@ public class HeritageItemHomepageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_heritage_item_homepage);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
 
         final Button newChoButton = (Button) findViewById(R.id.newChoButton);
 
@@ -37,6 +61,25 @@ public class HeritageItemHomepageActivity extends AppCompatActivity {
         String userToken  = getIntent().getStringExtra("userToken");
         intent.putExtra("userToken", userToken);
         startActivity(intent);
+    }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, CulturalHeritageObject>();
+
+        // Adding child data
+        listDataHeader.add("Topkapı Palace");
+        listDataHeader.add("Yerebatan Sarnıcı");
+        listDataHeader.add("İstanbul Modern");
+
+        // Adding child data
+        CulturalHeritageObject cho = new CulturalHeritageObject();
+        cho.setCategory("bla");
+        cho.setTitle("aaa");
+
+        listDataChild.put(listDataHeader.get(0), cho); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), cho);
+        listDataChild.put(listDataHeader.get(2), cho);
     }
 
 }
---
+++ Mobile/ANDROID/Herdot/app/src/main/java/com/ayranisthenewraki/heredot/herdot/model/ExpandableListAdapter.java
@@ -0,0 +1,169 @@
+package com.ayranisthenewraki.heredot.herdot.model;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ayranisthenewraki.heredot.herdot.HeritageItemHomepageActivity;
+import com.ayranisthenewraki.heredot.herdot.MapsViewAllActivity;
+import com.ayranisthenewraki.heredot.herdot.R;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by idilgun on 11/05/17.
+ */
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+
+    private Context _context;
+    private List<String> _listDataHeader; // header titles
+    // child data in format of header title, child title
+    private HashMap<String, CulturalHeritageObject> _listDataChild;
+
+    public ExpandableListAdapter(Context context, List<String> listDataHeader,
+                                 HashMap<String, CulturalHeritageObject> listChildData) {
+        this._context = context;
+        this._listDataHeader = listDataHeader;
+        this._listDataChild = listChildData;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosititon) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition));
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, final int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+
+        final CulturalHeritageObject cho = (CulturalHeritageObject) getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_body, null);
+        }
+
+        TextView listItemCategory = (TextView) convertView
+                .findViewById(R.id.listItemCategory);
+
+        TextView listItemDescription = (TextView) convertView
+                .findViewById(R.id.listItemDescription);
+
+        new DownloadImageTask((ImageView) convertView.findViewById(R.id.itemImageView))
+                .execute("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBK3FmyjIENz16NWEl1iJcIWj8I5n8hs-rl5JPixzw-XppNfKx");
+
+        listItemCategory.setText(cho.getCategory());
+        listItemDescription.setText("test");
+
+
+        Button viewOnMapButton = (Button) convertView.findViewById(R.id.itemViewOnMap);
+        viewOnMapButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(_context, MapsViewAllActivity.class);
+                Bundle bundle = new Bundle();
+                TimeLocationListWrapper tlcListWrapper = new TimeLocationListWrapper();
+                TimeLocationCouple tlc = new TimeLocationCouple();
+                List<TimeLocationCouple> tlcList = new ArrayList<TimeLocationCouple>();
+                tlcListWrapper.setTlcList(tlcList);
+                bundle.putSerializable("timeLocationList", tlcListWrapper);
+                intent.putExtras(bundle);
+                _context.startActivity(intent);
+            }
+        });
+
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return 1;
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this._listDataHeader.get(groupPosition);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this._listDataHeader.size();
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        String headerTitle = (String) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_header, null);
+        }
+
+        TextView lblListHeader = (TextView) convertView
+                .findViewById(R.id.lblListHeader);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerTitle);
+
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+}
