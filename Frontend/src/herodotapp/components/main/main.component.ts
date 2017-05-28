25c2896c02e44450b69700c6a927fcf252a5a82d
uyguronur <uyguronur@gmail.com>
2017-05-29 01:35:01 +0300

    Fixes #91 Hypothesis is loaded after view init

--- Frontend/src/herodotapp/components/main/main.component.ts
+++ Frontend/src/herodotapp/components/main/main.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
-    selector: 'main-app',
-    templateUrl: 'main.component.html'
+  moduleId: module.id,
+  selector: 'main-app',
+  templateUrl: 'main.component.html'
 })
-export class MainComponent implements OnInit{
-  ngOnInit () {
+export class MainComponent implements OnInit {
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    let appLinkEl = document.querySelector('link[type="application/annotator+html"]');
+    if (appLinkEl) appLinkEl.dispatchEvent(new Event('destroy'));
+
+    let embedScript = document.createElement('script');
+    embedScript.setAttribute('src', 'https://hypothes.is/app/embed.js');
+    document.getElementsByTagName("main-app")[0].appendChild(embedScript);
     (<any>window).hypothesisConfig = function () {
       return {
         "openSidebar": false,
@@ -15,3 +25,12 @@ export class MainComponent implements OnInit{
     };
   }
 }
+
+
+/*
+let appLinkEl = document.querySelector('link[type="application/annotator+html"]');
+if (appLinkEl) appLinkEl.dispatchEvent(new Event('destroy'));
+
+let embedScript = document.createElement('script');
+embedScript.setAttribute('src', 'https://hypothes.is/app/embed.js');
+document.getElementsByTagName("main-app")[0].appendChild(embedScript); */
